09c84752561645426af1fec3cdcb4000e9691f8d
UrmKevin <kevinurm2005@gmail.com>
2021-12-10 08:50:40 +0200

    *fixs*

--- Game21.py
+++ Game21.py
@@ -2,8 +2,10 @@ from random import*
 
 mode=""
 start=0
-p1_start=0
-p2_start=0
+p1_start=""
+p2_start=""
+r1=0
+r2=0
 
 while True:
     while type(mode)!=int():
@@ -22,33 +24,19 @@ while True:
                         if start.lower()=="y":
                             x=deck.pop(len(deck)-1)
                             P_score+=x
-                            if P_score==21:
-                                print("\nYou got",x,"points\nYou WON!\nYour score:",P_score,"\nBot's score:  ",B_score,"\n")
-                                break
-                            elif P_score>21:
-                                print("\nYou got",x,"points\nYou LOST!\nYour score:",P_score,"\nBot's score:  ",B_score,"\n")
-                                break
-                            else:
-                                print("\nYou got",x,"points\nYour score:",P_score)
                             y=deck.pop(len(deck)-1)
                             B_score+=y
-                            if B_score>21:
+                            if P_score==21 or B_score>21:
                                 print("\nYou got",x,"points\nYou WON!\nYour score:",P_score,"\nBot's score:  ",B_score,"\n")
                                 break
-                            elif B_score==21:
-                                print("\nYou got",x,"points\nYou LOST!\nYour score:",P_score,"\nBot's score:  ",B_score,"\n")
-                                break
-                        elif start.lower()=="n":
-                            y=deck.pop(len(deck)-1)
-                            B_score+=y
-                            if B_score>21:
-                                print("\nYou got",x,"points\nYou WON!\nYour score:  ",P_score,"\nBot's score:  ",B_score,"\n")
-                                break
-                            elif B_score==21:
+                            elif B_score==21 or P_score>21:
                                 print("\nYou got",x,"points\nYou LOST!\nYour score:",P_score,"\nBot's score:  ",B_score,"\n")
                                 break
                             else:
                                 print("\nYour score:",P_score)
+                        elif start.lower()=="n":
+                            print("\nYour score:",P_score,"\nGame is ended")
+                            break
                         else:
                             print("Please write 'n' or 'y'")
                     except:
@@ -62,38 +50,59 @@ while True:
                 print("START")
                 while True:
                     try:
-                        p1_start=input("\nPlayer 1 would you like to pick a card? y(yes)/n(no)\n")
+                        print("\nP1 score:",p1,"\nP2 score:",p2)
+                        while True:
+                            try:
+                                p1_start=input("\nPlayer 1 would you like to pick a card? y(yes)/n(no)\n").lower()
+                                if p1_start.lower()=="y":
+                                    break
+                                elif p1_start.lower()=="n":
+                                    break
+                            except:
+                                ValueError
                         if p1_start.lower()=="y":
                             x=deck.pop(len(deck)-1)
                             p1+=x
                             if p1==21:
-                                print("\nPlayer 1 got",x,"points\n\nPlayer 1 WON!\n\nPlayer's 1 score:",p1,"\nPlayer's 2 score:",p2,"\n")
+                                print("\nPlayer 1 got",y,"points\n\nPlayer 1 WON!\n\nPlayer's 1 score:",p1,"\nPlayer's 2 score:",p2,"\n")
+                                r1+=1
                                 break
                             elif p1>21:
-                                print("\nPlayer 1 got",x,"points\n\nPlayer 2 WON!\n\nPlayer's 1 score:",p1,"\nPlayer's 2 score:",p2,"\n")
+                                print("\nPlayer 1 got",y,"points\n\nPlayer 2 WON!\n\nPlayer's 1 score:",p1,"\nPlayer's 2 score:",p2,"\n")
+                                r2+=1
                                 break
                             else:
-                                print("\nPlayer 1 got",x,"points\nPlayer 1 score:",p1)
+                                print("\nPlayer's 1 score:",p1,"\nPlayer's 2 score:",p2)
                         elif p1_start.lower()=="n":
-                            print("\nPlayer's 1 score:",p1)
-                        else:
-                            print("Please write 'n' or 'y'")
-                        p2_start=input("\nPlayer 2 would you like to pick a card? y(yes)/n(no)\n")
+                            print("\nP1 score:",p1,"\nP2 score:",p2,"\nGame is ended\n")
+                            break
+                        print("\nP1 score:",p1,"\nP2 score:",p2)
+                        while True:
+                            try:
+                                p2_start=input("\nPlayer 1 would you like to pick a card? y(yes)/n(no)\n").lower()
+                                if p2_start.lower()=="y":
+                                    break
+                                elif p2_start.lower()=="n":
+                                    break
+                            except:
+                                ValueError
                         if p2_start.lower()=="y":
                             y=deck.pop(len(deck)-1)
                             p2+=y
+                            print("\nPlayer 2 got",y,"points\nPlayer's 2 score:",p2)
                             if p2==21:
                                 print("\nPlayer 2 got",y,"points\n\nPlayer 2 WON!\n\nPlayer's 1 score:",p1,"\nPlayer's 2 score:",p2,"\n")
+                                r2+=1
                                 break
                             elif p2>21:
                                 print("\nPlayer 2 got",y,"points\n\nPlayer 1 WON!\n\nPlayer's 1 score:",p1,"\nPlayer's 2 score:",p2,"\n")
+                                r1+=1
                                 break
                             else:
-                                print("\nPlayer 2 got",y,"points\nPlayer's 2 score:",p2)
-                        elif p1_start.lower()=="n":
-                            print("\nPlayer's 2 score:",p2)
-                        else:
-                            print("Please write 'n' or 'y'")
+                                print("\nPlayer's 1 score:",p1,"\nPlayer's 2 score:",p2)
+                        elif p2_start.lower()=="n":
+                            print("\nP1 score:",p1,"\nP2 score:",p2,"\nGame is ended\n")
+                            break
                     except:
                         ValueError
             elif mode==3:
